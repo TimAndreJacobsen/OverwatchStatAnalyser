9d6b3ddf8113dde7aa7d316d0338a16692258e56
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-06-26 23:48:05 +0200

    added types Map and Champion as List's to logic component

--- src/main/java/com/overwatch/statistics/gameround/logic/Logic.java
+++ src/main/java/com/overwatch/statistics/gameround/logic/Logic.java
@@ -1,12 +1,24 @@
 package com.overwatch.statistics.gameround.logic;
 
 import com.overwatch.statistics.gameround.GameRound;
+import com.overwatch.statistics.gameround.model.Champion;
+import com.overwatch.statistics.gameround.model.Map;
 
 import java.util.List;
 
 public class Logic {
 
     private List<GameRound> gameRounds;
+    private List<Map> mapList;
+    private List<Champion> championList;
+
+    // Setters
+    public void setMapList(List<Map> mapList) {
+        this.mapList = mapList;
+    }
+    public void setChampionList(List<Champion> championList) {
+        this.championList = championList;
+    }
 
     public void loadGameRounds(List<GameRound> gameRounds) {
         this.gameRounds = gameRounds;
@@ -14,18 +26,28 @@ public class Logic {
 
     public void calculateSkillRatingChange() {
         for (int i = 1; i < gameRounds.size(); i++) {
-            int previousRound = gameRounds.get(i-1).getSkillRating();
+            int previousRound = gameRounds.get(i - 1).getSkillRating();
             int thisRound = gameRounds.get(i).getSkillRating();
             gameRounds.get(i).setSkillRatingChange(thisRound - previousRound);
         }
     }
 
-//    public double calculateWinRate(String map, String champ) {
-//
-//        for (Object o : ) {
-//
-//        }
-//
-//        return 0.5;
-//    }
+    public double calculateWinRate(String map, String champ) {
+        int roundsPlayed = 0;
+        int roundsWon = 0;
+
+        for (GameRound r : gameRounds) {
+            if (r.getMapName().equalsIgnoreCase(map)
+                    && r.getChampion().equalsIgnoreCase(champ)) {
+
+                roundsPlayed++;
+                if (r.isWin()) {
+                    roundsWon++;
+                }
+            }
+        }
+        return roundsPlayed / roundsWon;
+    }
+
+
 }
