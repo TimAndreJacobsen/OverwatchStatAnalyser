0934c10ba3f2b07a81da94e096092aa13403a6be
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-06-26 23:44:21 +0200

    Created object Champion to encapsulate associated data

---
+++ src/main/java/com/overwatch/statistics/gameround/model/Champion.java
@@ -0,0 +1,13 @@
+package com.overwatch.statistics.gameround.model;
+
+public class Champion {
+
+    private String name;
+    private String role;
+    private double winRate;
+
+    public Champion(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+}
