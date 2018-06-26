0ede8eff93c080fe5bc7c3aaee20995d8870124f
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-06-26 23:44:48 +0200

    created Type Map to encapsulate associated data

---
+++ src/main/java/com/overwatch/statistics/gameround/model/Map.java
@@ -0,0 +1,12 @@
+package com.overwatch.statistics.gameround.model;
+
+public class Map {
+
+    private String name;
+    private String type;
+
+    public Map(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+}
