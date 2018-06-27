0dc25d10d1c51b35b2a7f49176d5f7f7a913c44c
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-06-27 23:34:43 +0200

    GameRound now makes use of the new types Map and Champion

--- src/main/java/com/overwatch/statistics/gameround/GameRound.java
+++ src/main/java/com/overwatch/statistics/gameround/GameRound.java
@@ -1,43 +1,40 @@
 package com.overwatch.statistics.gameround;
 
+import com.overwatch.statistics.gameround.model.Champion;
+import com.overwatch.statistics.gameround.model.Map;
+
 public class GameRound {
 
     private int skillRatingChange;
     private int skillRating;
-    private String champion;
     private boolean isWin;
-    private String mapName;
-    private String mapType;
     private String date;
     private String audioType;
+    private Map map;
+    private Champion champion;
 
     public GameRound() {
         this.skillRating = -1;
-        this.champion = "";
+        this.champion = null;
         this.isWin = false;
-        this.mapName = "";
-        this.mapType = "";
+        this.map = null;
         this.date = "";
         this.audioType = "";
         this.skillRatingChange = 0;
     }
 
-
     // Getters
     public int getSkillRating() {
         return skillRating;
     }
-    public String getChampion() {
-        return champion;
+    public Champion getChampion() {
+        return this.champion;
     }
     public boolean isWin() {
         return isWin;
     }
-    public String getMapName() {
-        return mapName;
-    }
-    public String getMapType() {
-        return mapType;
+    public Map getMap() {
+        return map;
     }
     public String getAudioType() {
         return audioType;
@@ -50,17 +47,14 @@ public class GameRound {
     public void setSkillRating(int skillRating) {
         this.skillRating = skillRating;
     }
-    public void setChampion(String champion) {
+    public void setChampion(Champion champion) {
         this.champion = champion;
     }
-    public void setWin(boolean win) {
+    public void setIsWin(boolean win) {
         isWin = win;
     }
-    public void setMapName(String mapName) {
-        this.mapName = mapName;
-    }
-    public void setMapType(String mapType) {
-        this.mapType = mapType;
+    public void setMap(Map map) {
+        this.map = map;
     }
     public void setAudioType(String audioType) {
         this.audioType = audioType;
@@ -71,12 +65,5 @@ public class GameRound {
     public void setSkillRatingChange(int skillRatingChange) {
         this.skillRatingChange = skillRatingChange;
     }
-//    public void setSkillRatingAfter(int skillRatingAfter) {
-//        this.skillRatingAfter = skillRatingAfter;
-//    }
-//    public void setSkillRatingDelta(int skillRatingDelta) {
-//        this.skillRatingDelta = skillRatingDelta;
-//    }
-
 
 }
