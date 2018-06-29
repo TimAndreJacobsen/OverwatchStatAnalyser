952a2a2eda1afa4d59182e0f0291f5534a5615c7
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-06-29 23:27:34 +0200

    added new fields to Champion. Champions now know: Their own total win rate, The different maps they can go to, their win rate on a per map basis(soon tm).

--- src/main/java/com/overwatch/statistics/gameround/model/Champion.java
+++ src/main/java/com/overwatch/statistics/gameround/model/Champion.java
@@ -1,13 +1,26 @@
 package com.overwatch.statistics.gameround.model;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class Champion {
 
     private String name;
     private String role;
-    private double winRate;
+    private double totalWinRate;
 
-    public Champion(String name, String role) {
+    private Set<Map> maps;
+    private HashMap<Map, Double> winRateEachMap;
+
+    Champion(String name, String role, Set<Map> maps) {
         this.name = name;
         this.role = role;
+        this.totalWinRate = 0;
+        this.winRateEachMap = new HashMap<>();
+        this.maps = maps;
+
+        for (Map m : maps) {
+            winRateEachMap.put(m, 0.0);
+        }
     }
 }
