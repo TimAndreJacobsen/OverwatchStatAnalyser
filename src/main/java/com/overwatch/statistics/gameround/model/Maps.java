a82cc310ea7b6ae5dd94b4deda959a9fb2332b75
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-06-29 21:42:31 +0200

    Map initialization is now being done in its own Maps class

---
+++ src/main/java/com/overwatch/statistics/gameround/model/Maps.java
@@ -0,0 +1,45 @@
+package com.overwatch.statistics.gameround.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Maps {
+
+    private Set<Map> maps = new HashSet<>();
+    private Map blizzardWorld = new Map("Blizzard World", "Hybrid Payload");
+    private Map dorado = new Map("Dorado", "Payload");
+    private Map eichenwalde = new Map("Eichenwalde", "Hybrid Payload");
+    private Map hanamura = new Map("Hanamura", "2 Capture Points");
+    private Map junkertown = new Map("Junkertown", "Payload");
+    private Map kingsRow = new Map("Kings Row", "Hybrid Payload");
+    private Map liangTowers = new Map("Liang Towers", "King of the Hill");
+    private Map lunarColony = new Map("Lunar Colony", "2 Capture Points");
+    private Map nepal = new Map("Nepal", "King of the Hill");
+    private Map rialto = new Map("Rialto", "Payload");
+    private Map route66 = new Map("Route 66", "Payload");
+    private Map templeOfAnubis = new Map("Temple of Anubis", "2 Capture Points");
+    private Map volskayaIndustries = new Map("Volskaya Industries", "2 Capture Points");
+
+    // Getters
+    public Set<Map> getMaps() {
+        return maps;
+    }
+
+    // Populate maps set
+    public void addMapsToList() {
+        maps.add(blizzardWorld);
+        maps.add(dorado);
+        maps.add(eichenwalde);
+        maps.add(hanamura);
+        maps.add(junkertown);
+        maps.add(kingsRow);
+        maps.add(liangTowers);
+        maps.add(lunarColony);
+        maps.add(nepal);
+        maps.add(rialto);
+        maps.add(route66);
+        maps.add(templeOfAnubis);
+        maps.add(volskayaIndustries);
+    }
+
+}
