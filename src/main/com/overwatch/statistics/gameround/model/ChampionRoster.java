f76dd87a3e631ca7c6f63384a734c09932b177a6
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-02-08 01:14:45 +0100

    factored out champion initialization

--- src/main/com/overwatch/statistics/gameround/model/ChampionRoster.java
+++ src/main/com/overwatch/statistics/gameround/model/ChampionRoster.java
@@ -7,6 +7,10 @@ public class ChampionRoster {
 
     private Set<Champion> champions = new HashSet<>();
     private Set<Map> maps = new HashSet<>();
+    private Champion ana = new Champion("Ana", "Support", maps);
+    private Champion brigitte = new Champion("Brigitte", "Support", maps);
+    private Champion zenyatta = new Champion("Zenyatta", "Support", maps);
+    private Champion other = new Champion("Other", "Other", maps);
 
     // Getters
     public Set<Champion> getChampions() {
@@ -23,11 +27,6 @@ public class ChampionRoster {
 
     // populate Champions set
     public void addChampionsToList() {
-        Champion ana = new Champion("Ana", "Support", maps);
-        Champion brigitte = new Champion("Brigitte", "Support", maps);
-        Champion zenyatta = new Champion("Zenyatta", "Support", maps);
-        Champion other = new Champion("Other", "Other", maps);
-
         champions.add(ana);
         champions.add(brigitte);
         champions.add(zenyatta);
