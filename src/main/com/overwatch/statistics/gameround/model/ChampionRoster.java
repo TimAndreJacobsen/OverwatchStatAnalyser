b14b5f72a0dc6e544ad54dc4e601cd2927280ce9
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-04-08 10:31:02 +0200

    added rest of support heros

--- src/main/com/overwatch/statistics/gameround/model/ChampionRoster.java
+++ src/main/com/overwatch/statistics/gameround/model/ChampionRoster.java
@@ -7,7 +7,12 @@ public class ChampionRoster {
 
     private Set<Champion> champions = new HashSet<>();
     private Set<Map> maps = new HashSet<>();
+
     private Champion ana = new Champion("Ana", "Support", maps);
+    private Champion baptiste = new Champion("Baptiste", "Support", maps);
+    private Champion lucio = new Champion("Lucio", "Support", maps);
+    private Champion mercy = new Champion("Mercy", "Support", maps);
+    private Champion moira = new Champion("Moira", "Support", maps);
     private Champion brigitte = new Champion("Brigitte", "Support", maps);
     private Champion zenyatta = new Champion("Zenyatta", "Support", maps);
     private Champion other = new Champion("Other", "Other", maps);
@@ -26,10 +31,22 @@ public class ChampionRoster {
     }
 
     // populate Champions set
-    public void addChampionsToList() {
+    private void addChampionsToList() {
+
+        // supports
         champions.add(ana);
         champions.add(brigitte);
         champions.add(zenyatta);
+        champions.add(baptiste);
+        champions.add(lucio);
+        champions.add(mercy);
+        champions.add(moira);
         champions.add(other);
+    }
+
+    private void supportChampions() {
+        champions.add(ana);
+        champions.add(brigitte);
+        champions.add(zenyatta);
     }
 }
