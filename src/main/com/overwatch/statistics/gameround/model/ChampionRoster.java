d6235c13354fdc36bf2a7104fc6877c1b4ee25dc
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-04-08 11:00:46 +0200

    added dps champions

--- src/main/com/overwatch/statistics/gameround/model/ChampionRoster.java
+++ src/main/com/overwatch/statistics/gameround/model/ChampionRoster.java
@@ -24,7 +24,22 @@ public class ChampionRoster {
     private Champion wreckingBall = new Champion("Wrecking Ball", "Tank", maps);
     private Champion zarya = new Champion("Zarya", "Tank", maps);
 
-
+    private Champion ashe = new Champion("Ashe", "DPS", maps);
+    private Champion bastion = new Champion("Bastion", "DPS", maps);
+    private Champion doomfist = new Champion("Doomfist", "DPS", maps);
+    private Champion genji = new Champion("Genji", "DPS", maps);
+    private Champion hanzo = new Champion("Hanzo", "DPS", maps);
+    private Champion junkrat = new Champion("Junkrat", "DPS", maps);
+    private Champion mccree = new Champion("McCree", "DPS", maps);
+    private Champion mei = new Champion("Mei", "DPS", maps);
+    private Champion pharah = new Champion("Pharah", "DPS", maps);
+    private Champion reaper = new Champion("Reaper", "DPS", maps);
+    private Champion soldier76 = new Champion("Soldier: 76", "DPS", maps);
+    private Champion sombra = new Champion("Sombra", "DPS", maps);
+    private Champion symmetra = new Champion("Symmetra", "DPS", maps);
+    private Champion torbjorn = new Champion("Torbjorn", "DPS", maps);
+    private Champion tracer = new Champion("Tracer", "DPS", maps);
+    private Champion widowmaker = new Champion("Widowmaker", "DPS", maps);
 
     private Champion other = new Champion("Other", "Other", maps);
 
@@ -63,7 +78,22 @@ public class ChampionRoster {
         champions.add(zarya);
 
         // dps
-
+        champions.add(ashe);
+        champions.add(bastion);
+        champions.add(doomfist);
+        champions.add(genji);
+        champions.add(hanzo);
+        champions.add(junkrat);
+        champions.add(mccree);
+        champions.add(mei);
+        champions.add(pharah);
+        champions.add(reaper);
+        champions.add(soldier76);
+        champions.add(sombra);
+        champions.add(symmetra);
+        champions.add(torbjorn);
+        champions.add(tracer);
+        champions.add(widowmaker);
 
         champions.add(other);
     }
